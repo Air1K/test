e351d49254f7cb4bdb781d0031cacbe4b36cf2c5
Maxim Veremeenko <mvv@vt2b.ru>
2024-12-05 22:15:55 +0500

    feat: PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import App from './App.vue';
+import router from './router';
+import Aura from '@primevue/themes/aura';
+const app = createApp(App);
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.my-app-dark',
+    },
+  },
+});
+app.mount('#app');
